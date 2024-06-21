17f2420b88e8623ea7847792b4d3d0dbb96fb457
T.Ivanov2304 <t.ivanov2304@gmail.com>
2024-06-21 19:40:23 +0300

    Created MiniORM for SoftUni workshop

---
+++ Entity Framework/1.ORM Fundamentals/01.ORM-Fundamentals-Exercise-MiniORM-Skeleton-6.0/MiniORM.App/Entities/EmployeeProject.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniORM.App.Entities;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class EmployeeProject
+{
+    [Key, ForeignKey(nameof(Employee))] public int EmployeeID { get; set; }
+    public Employee Employee { get; set; }
+
+    [Key, ForeignKey(nameof(Project))] public int ProjectID { get; set; }
+    public Project Project { get; set; }
+}
---
+++ Entity Framework/1.ORM Fundamentals/01.ORM-Fundamentals-Exercise-MiniORM-Skeleton-6.0/MiniORM.App/Program.cs
@@ -0,0 +1,19 @@
+﻿using MiniORM.App;
+using MiniORM.App.Entities;
+
+const string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=SoftUni;Trusted_Connection=True;TrustServerCertificate = true";
+var dbContext = new SoftUniDbContext(connectionString);
+
+Console.WriteLine();
+// Add
+//dbContext.Projects.Add(new Project { Name = "MiniORM" });
+
+// Remove
+var projectToDelete = dbContext.Projects.FirstOrDefault(x => x.Name == "MiniORM");
+if (projectToDelete is not null) dbContext.Projects.Remove(projectToDelete);
+
+// Update
+var employeesToUpdate = dbContext.Employees.Where(e => e.LastName == "Taylor" || !e.IsEmployed).ToList();
+foreach (var employee in employeesToUpdate) employee.LastName = "Smith";
+
+dbContext.SaveChanges();
